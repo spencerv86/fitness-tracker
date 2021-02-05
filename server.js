df3e42916e1bcbfa3bc6ec030fd37f45a9fbd715
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-05 14:44:38 -0500

    required the workoutcontroller in the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Workout = require("./models/Workout");
 
 const PORT = process.env.PORT || 8080;
 
@@ -11,12 +12,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
+app.use(require("./controllers/WorkoutController"));
+
 app.listen(PORT, () => {
-    console.log("App running on http://localhost:"+PORT);
+  console.log("App running on http://localhost:" + PORT);
 });
