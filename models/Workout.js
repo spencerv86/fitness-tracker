28b65fc5b984dbe08a75af14bc4beefa286b54ec
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-05 14:44:01 -0500

    adjusted the exercises array so that each item was defined in an object

--- models/Workout.js
+++ models/Workout.js
@@ -9,13 +9,27 @@ const workoutSchema = new Schema({
   },
   exercises: [
     {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
-      distance: Number
+      type: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
   ],
 });
