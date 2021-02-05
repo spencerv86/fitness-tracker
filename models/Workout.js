97025347e0014a10a9ee041a5f1d6fbac23c1a00
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-05 14:24:26 -0500

    built the Workout model for reference

---
+++ models/Workout.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+  exercises: [
+    {
+      type: String,
+      name: String,
+      duration: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
