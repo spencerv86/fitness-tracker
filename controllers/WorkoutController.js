3d82b1f7f45ef10fff2d8982a8379f107eeeefca
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-07 14:22:55 -0500

    created the put route to update a workout by id

--- controllers/WorkoutController.js
+++ controllers/WorkoutController.js
@@ -16,6 +16,7 @@ router.get("/api/workouts", (req, res) => {
 router.post("/api/workouts", (req, res) => {
   Workout.create(req.body)
     .then((dbWorkout) => {
+      console.log(JSON.parse(dbWorkout));
       res.json(dbWorkout);
     })
     .catch((err) => {
@@ -23,6 +24,13 @@ router.post("/api/workouts", (req, res) => {
     });
 });
 
-
+router.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    Workout.findOneAndUpdate(id, {$push: {exercises: req.body}}).then((updatedWorkout) => {
+      res.json(updatedWorkout);
+    }).catch((err) => {
+      res.json(err);
+    })
+})
 
 module.exports = router;
