a1ab63ed96bceb0679545d15fd226e9873936802
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-08 17:39:15 -0500

    adjusted the mongoose connection in the seeds file

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,11 @@
 let mongoose = require("mongoose");
 let Workout = require("../models/Workout");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/savedworkouts", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 let workoutSeed = [
