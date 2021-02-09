f78704814e729f95829be6755e96a751eea8851c
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-08 21:24:32 -0500

    removed errant '/' route and created the range route, amended put route

--- controllers/WorkoutController.js
+++ controllers/WorkoutController.js
@@ -6,9 +6,9 @@ const path = require("path");
 // HTML Routes
 
 // Routes to the homepage
-router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// router.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 // Routes to the stats page through stats.html
 router.get("/stats", function (req, res) {
@@ -33,10 +33,22 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ day: -1 })
+    .limit(7)
+    .then((workoutRange) => {
+      res.json(workoutRange);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 router.post("/api/workouts", (req, res) => {
   Workout.create(req.body)
     .then((dbWorkout) => {
-      console.log(JSON.parse(dbWorkout));
+      // console.log(JSON.parse(dbWorkout));
       res.json(dbWorkout);
     })
     .catch((err) => {
@@ -47,7 +59,7 @@ router.post("/api/workouts", (req, res) => {
 // Edits a specific workout by ID
 router.put("/api/workouts/:id", (req, res) => {
   let id = req.params.id;
-  Workout.findOneAndUpdate(id, { $push: { exercises: req.body } })
+  Workout.findOneAndUpdate({ _id: id }, { $push: { exercises: req.body } })
     .then((updatedWorkout) => {
       res.json(updatedWorkout);
     })
