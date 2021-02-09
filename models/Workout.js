2d09843e54654816129fb513cce6b0bdcd211d6a
Spencer Vaughan <spencer.vaughan@gmail.com>
2021-02-08 21:23:36 -0500

    created totalDuration virtual

--- models/Workout.js
+++ models/Workout.js
@@ -2,36 +2,51 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  date: {
-    type: Date,
-    default: Date.now(),
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-      },
-      name: {
-        type: String,
-      },
-      duration: {
-        type: Number,
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-      distance: {
-        type: Number,
-      },
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now,
     },
-  ],
+    exercises: [
+      {
+        type: {
+          type: String,
+        },
+        name: {
+          type: String,
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+workoutSchema.virtual("totalDuration").get(function() {
+  let total = 0;
+  for (let i = 0; i < this.exercises.length; i++){
+    total += this.exercises[i].duration;
+  }
+  return total;
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
